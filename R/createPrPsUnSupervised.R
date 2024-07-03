e70c9fda8e82ae195de9fdd75be9dc467d480ab2
RMolania <ramyar.molania@gmail.com>
2024-07-03 15:10:42 +1000

    major revision

--- R/createPrPsUnSupervised.R
+++ R/createPrPsUnSupervised.R
@@ -1,4 +1,4 @@
-#' create unsupervised pseudo-replicates of pseudo samples (PRPS).
+#' Create unsupervised pseudo-replicates of pseudo samples (PRPS).
 
 #' @author Ramyar Molania
 
@@ -10,9 +10,9 @@
 #' @param assay.name Symbol. A symbol indicating the assay name within the SummarizedExperiment object for the creation
 #' of PRPS data. The assay must be the one that will be used as data input for the RUV-III-PRPS normalization.
 #' @param approach Symbol. Indicates which method to be used. The options are 'anchor' and 'mnn'.
-#' @param uv.variables Symbol. A symbol specifying the name of columns in the sample annotation of the SummarizedExperiment
-#' object. This variable can to be categorical or continuous variable. If a continuous variable is provide, this will be
-#' divided into groups using the clusteintf method.
+#' @param uv.variables Symbol. A symbol or a vector of symbols specifying the name(s) of column(s) in the sample annotation
+#' of the SummarizedExperiment object. This variable can to be categorical or continuous variable. If a continuous variable
+#' is provide, this will be divided into groups using the clustering method.
 #' @param clustering.method Symbol.A symbol indicating the choice of clustering method for grouping the 'uv.variable'
 #' if a continuous variable is provided. Options include 'kmeans', 'cut', and 'quantile'. The default is set to 'kmeans'.
 #' @param nb.clusters Numeric. A numeric value indicating how many clusters should be found if the 'uv.variable' is a
@@ -23,13 +23,12 @@
 #' The default is 2000. We refer to the FindIntegrationAnchors R function for more details.
 #' @param anchor.scale Logical. Whether or not to scale the features provided. Only set to FALSE if you have previously scaled
 #' the features you want to use for each object in the object.list.
-#' @param min.prps.samples Numeric. The minimum number of samples to be averaged to create a pseudo-sample. The default
+#' @param filter.prps.sets TTTT
+#' @param min.ps.samples Numeric. The minimum number of samples to be averaged to create a pseudo-sample. The default
 #' is 3. The minimum value is 2.
-#' @param max.prps.samples Numeric value indicating the maximum number of samples to be averaged for creating a pseudo-sample.
+#' @param max.ps.samples Numeric value indicating the maximum number of samples to be averaged for creating a pseudo-sample.
 #' The default is 'inf'. Please note that averaging a high number of samples may lead to inaccurate PRPS estimates.
 #' @param max.prps.sets Numeric. The maximum number of PRPS sets across batches. The default in 10.
-#' @param min.score Numeric. A cut off to filter anchors based the scores that the FindIntegrationAnchors calculates.
-#' The default is NULL, indicating no filtration will be applied based on the scores. The value is between 0 or 1.
 #' @param normalization.method Symbol. Indicate which normalization methods should be used before finding the anchors.
 #' The options are "LogNormalize" or "SCT". The default is "LogNormalize".
 #' @param sct.clip.range Numeric. Numeric of length two specifying the min and max values the Pearson residual will be
@@ -61,9 +60,11 @@
 #' @param svd.bsparam A BiocParallelParam object specifying how parallelization should be performed. The default is bsparam().
 #' We refer to the 'runSVD' function from the BiocSingular R package.
 #' @param k.nn Numeric.The maximum number of nearest neighbors to compute. The default is set 3.
+#' @param mnn Numeric.The maximum number of nearest neighbors to compute. The default is set 3.
 #' @param normalization Symbol. Indicates which normalization methods should be applied before finding the knn. The default
 #' is 'cpm'. If is set to NULL, no normalization will be applied.
-#' @param regress.out.bio.variables Symbols. Indicates the columns names that contain biological variables in the
+#' @param apply.cosine.norm TTTT
+#' @param regress.out.variables Symbols. Indicates the columns names that contain biological variables in the
 #' SummarizedExperiment object. These variables will be regressed out from the data before finding genes that are highly
 #' affected by unwanted variation variable. The default is NULL, indicates the regression will not be applied.
 #' @param apply.log Logical. Indicates whether to apply a log-transformation to the data or not. The default is 'TRUE'.
@@ -71,6 +72,8 @@
 #' log transformation to avoid -Inf for raw counts that are equal to 0. The default is 1.
 #' @param mnn.bpparam Symbol. A BiocParallelParam object specifying how parallelization should be performed to find MNN.
 #' . The default is SerialParam(). We refer to the 'findMutualNN' function from the 'BiocNeighbors' R package.
+#' @param mnn.nbparam Symbol. A BiocParallelParam object specifying how parallelization should be performed to find MNN.
+#' . The default is KmknnParam(). We refer to the 'findMutualNN' function from the 'BiocNeighbors' R package.
 #' @param assess.se.obj Logical. Indicates whether to assess the SummarizedExperiment object or not. See the checkSeObj
 #' function for more details.
 #' @param remove.na Symbol. To remove NA or missing values from the assays or not. The options are 'assays' and 'none'.
@@ -78,7 +81,8 @@
 #' the checkSeObj function for more details.
 #' @param save.se.obj Logical. Indicates whether to save the RLE results in the metadata of the SummarizedExperiment
 #' object or to output the result as list. By default it is set to TRUE.
-#' @param  prps.group Symbol.
+#' @param output.name Symbol.
+#' @param prps.group Symbol.
 #' @param verbose Logical. If 'TRUE', shows the messages of different steps of the function.
 
 #' @importFrom SummarizedExperiment assay colData
@@ -96,10 +100,10 @@ createPrPsUnSupervised <- function(
         hvg = NULL,
         anchor.features = 2000,
         anchor.scale = TRUE,
-        min.prps.samples = 3,
-        max.prps.samples = 'inf',
+        filter.prps.sets = TRUE,
+        min.ps.samples = 3,
+        max.ps.samples = 'inf',
         max.prps.sets = 10,
-        min.score = NULL,
         normalization.method = "LogNormalize",
         sct.clip.range = NULL,
         reduction = "cca",
@@ -117,18 +121,22 @@ createPrPsUnSupervised <- function(
         center = TRUE,
         scale = FALSE,
         svd.bsparam = bsparam(),
+        mnn = 1,
         k.nn = 2,
         normalization = 'CPM',
-        regress.out.bio.variables = NULL,
+        apply.cosine.norm = FALSE,
+        regress.out.variables = NULL,
         apply.log = TRUE,
         pseudo.count = 1,
         mnn.bpparam = SerialParam(),
+        mnn.nbparam = KmknnParam(),
         assess.se.obj = TRUE,
         remove.na = 'both',
         save.se.obj = TRUE,
+        output.name = NULL,
         prps.group = NULL,
         verbose = TRUE
-) {
+        ){
     if (approach == 'anchor') {
         for (i in uv.variables) {
             se.obj <- createPrPsByAnchors(
@@ -142,10 +150,9 @@ createPrPsUnSupervised <- function(
                 pseudo.count = pseudo.count,
                 anchor.features = anchor.features,
                 scale = anchor.scale,
-                min.prps.samples = min.prps.samples,
-                max.prps.samples = max.prps.samples,
+                min.ps.samples = min.ps.samples,
+                max.ps.samples = max.ps.samples,
                 max.prps.sets = max.prps.sets,
-                min.score = min.score,
                 normalization.method = normalization.method,
                 sct.clip.range = sct.clip.range,
                 reduction = reduction,
@@ -161,18 +168,21 @@ createPrPsUnSupervised <- function(
                 assess.se.obj = assess.se.obj,
                 remove.na = remove.na,
                 save.se.obj = save.se.obj,
+                output.name = output.name,
                 prps.group = prps.group,
                 verbose = verbose
             )
         }
     }
-    if (approach == 'mnn') {
+    if (approach == 'knn.mnn') {
         for (i in uv.variables) {
-            se.obj <- createPrPsByMnn (
+            se.obj <- createPrPsByKnnMnn (
                 se.obj = se.obj,
                 assay.name = assay.name,
                 uv.variable = i,
                 data.input = data.input,
+                filter.prps.sets = filter.prps.sets,
+                max.prps.sets = max.prps.sets,
                 nb.pcs = nb.pcs,
                 center = center,
                 scale = scale,
@@ -180,15 +190,45 @@ createPrPsUnSupervised <- function(
                 clustering.method = clustering.method,
                 nb.clusters = nb.clusters,
                 k.nn = k.nn,
+                mnn = mnn,
                 hvg = hvg,
                 normalization = normalization,
-                regress.out.bio.variables = regress.out.bio.variables,
+                apply.cosine.norm = apply.cosine.norm,
+                regress.out.variables = regress.out.variables,
                 apply.log = apply.log,
                 pseudo.count = pseudo.count,
                 mnn.bpparam = mnn.bpparam,
                 assess.se.obj = assess.se.obj,
                 remove.na = remove.na,
                 save.se.obj = save.se.obj,
+                prps.group = prps.group,
+                verbose = verbose
+            )
+        }
+    }
+    if (approach == 'mnn') {
+        for (i in uv.variables) {
+            se.obj <- createPrPsByMnn(
+                se.obj = se.obj,
+                assay.name = assay.name,
+                uv.variable = i,
+                filter.prps.sets = filter.prps.sets,
+                max.prps.sets = max.prps.sets,
+                clustering.method = clustering.method,
+                nb.clusters = nb.clusters,
+                mnn = mnn,
+                hvg = hvg,
+                normalization = normalization,
+                apply.cosine.norm = apply.cosine.norm,
+                regress.out.variables = regress.out.variables,
+                apply.log = apply.log,
+                pseudo.count = pseudo.count,
+                mnn.bpparam = mnn.bpparam,
+                mnn.nbparam = mnn.nbparam,
+                assess.se.obj = assess.se.obj,
+                remove.na = remove.na,
+                save.se.obj = save.se.obj,
+                output.name = output.name,
                 prps.group = prps.group,
                 verbose = verbose
             )
