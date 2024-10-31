66529905e767f43cb38c6d5f599cf28fa2c00d07
RMolania <ramyar.molania@gmail.com>
2024-10-31 14:44:00 -0400

    fix factor levels

--- R/computeDGE.R
+++ R/computeDGE.R
@@ -1,4 +1,4 @@
-#' Perform differential gene expression analysis using the Wilcoxon test.
+#' Perform differential gene expression analysis.
 
 #' @author Ramyar Molania
 
@@ -18,7 +18,9 @@
 #' SummarizedExperiment object will be selected.
 #' @param variable Symbol. A symbol that indicates a column name of the SummarizedExperiment object that contains a
 #' categorical variable such as batches. If the variable has more than two levels, the function perform DGE between all
-#' possible pariwise groups.
+#' possible pairwise groups.
+#' @param method Symbol. A symbol that indicates which DE method should be used. Options are 'limma' and 'Wilcoxon'. The
+#' defalut is set to 'limma'.
 #' @param apply.log Logical. Indicates whether to apply a log-transformation to the data before compuritn DGE. The
 #' default is 'TRUE'.
 #' @param pseudo.count Numeric. A value as a pseudo count to be added to all measurements before log transformation. The
@@ -40,11 +42,14 @@
 
 #' @importFrom SummarizedExperiment assays assay
 #' @importFrom matrixTests row_wilcoxon_twosample
+#' @importFrom limma makeContrasts lmFit contrasts.fit eBayes
+#' @importFrom stats model.matrix
 #' @export
 
 computeDGE <- function(
         se.obj,
         assay.names = 'all',
+        method = 'limma',
         variable,
         apply.log = TRUE,
         pseudo.count = 1,
@@ -112,10 +117,10 @@ computeDGE <- function(
         }
 
         # Assess the SummarizedExperiment object ####
-        if (assess.se.obj) {
+        if (isTRUE(assess.se.obj)) {
             se.obj <- checkSeObj(
                 se.obj = se.obj,
-                assay.names = assay.names,
+                assay.names = levels(assay.names),
                 variables = variable,
                 remove.na = remove.na,
                 verbose = verbose
@@ -127,60 +132,111 @@ computeDGE <- function(
             message = '-- Data log transformation:',
             color = 'magenta',
             verbose = verbose
-        )
+            )
         all.assays <- applyLog(
             se.obj = se.obj,
-            assay.names = assay.names,
+            assay.names = levels(assay.names),
             apply.log = apply.log,
             pseudo.count = pseudo.count,
             assessment = 'DGE',
             verbose = verbose
-        )
+            )
         # Apply Wilcoxon test ####
-        printColoredMessage(
-            message = paste0('-- Perform Wilcoxon test between all possible contrasts () of the', variable, 'variable.') ,
-            color = 'magenta',
-            verbose = verbose
-        )
-        all.contrasts <- combn(
-            x = unique(colData(se.obj)[[variable]]),
-            m = 2
-        )
-        all.wilcoxon.tests <- lapply(
-            levels(assay.names),
-            function(x){
-                printColoredMessage(
-                    message = paste0('- Apply the Wilcoxon test on the "', x, '" data:'),
-                    color = 'orange',
-                    verbose = verbose
+        if(method == 'wilcoxon'){
+            printColoredMessage(
+                message = paste0(
+                    '-- Perform Wilcoxon test between all possible contrasts of the ',
+                    variable,
+                    ' variable.') ,
+                color = 'magenta',
+                verbose = verbose
+            )
+            all.contrasts <- combn(
+                x = unique(colData(se.obj)[[variable]]),
+                m = 2
+            )
+            all.wilcoxon.tests <- lapply(
+                levels(assay.names),
+                function(x){
+                    printColoredMessage(
+                        message = paste0('- Apply the Wilcoxon test on the "', x, '" data:'),
+                        color = 'orange',
+                        verbose = verbose
+                    )
+                    de.results <- lapply(
+                        1:ncol(all.contrasts),
+                        function(i){
+                            printColoredMessage(
+                                message = paste0(
+                                    '* Wilcoxon test between the ', all.contrasts[1 , i], ' and ', all.contrasts[2 , i],'.'),
+                                color = 'blue',
+                                verbose = verbose
+                            )
+                            data.x <- all.assays[[x]][ , colData(se.obj)[[variable]] == all.contrasts[1 , i] ]
+                            data.y <- all.assays[[x]][ , colData(se.obj)[[variable]] == all.contrasts[2 , i] ]
+                            de.table <- matrixTests::row_wilcoxon_twosample(data.x, data.y)[ , c('obs.x', 'obs.y', 'pvalue')]
+                        })
+                    names(de.results) <- sapply(
+                        1:ncol(all.contrasts),
+                        function (x)
+                            paste(all.contrasts[1 , x], all.contrasts[2 , x], sep = '&')
+                    )
+                    return(de.results)
+                })
+            names(all.wilcoxon.tests) <- levels(assay.names)
+        }
+        if(method == 'limma'){
+            printColoredMessage(
+                message = paste0(
+                    '-- Perform limma between all possible contrasts of the ',
+                    variable,
+                    ' variable.') ,
+                color = 'magenta',
+                verbose = verbose
+            )
+            de.design <- model.matrix(~ 0 + se.obj[[variable]])
+            colnames(de.design) <- levels(factor(se.obj[[variable]]))
+            all.possible.contrasts <- combn(x = colnames(de.design), m = 2)
+            all.possible.contrasts <- sapply(
+                1:ncol(all.possible.contrasts),
+                function(x) paste0(all.possible.contrasts[,x], collapse = '-')
                 )
-                de.results <- lapply(
-                    1:ncol(all.contrasts),
-                    function(i){
-                        printColoredMessage(
-                            message = paste0(
-                                '* Wilcoxon test between the ', all.contrasts[1 , i], ' and ', all.contrasts[2 , i],'.'),
-                            color = 'blue',
-                            verbose = verbose
-                        )
-                        data.x <- all.assays[[x]][ , colData(se.obj)[[variable]] == all.contrasts[1 , i] ]
-                        data.y <- all.assays[[x]][ , colData(se.obj)[[variable]] == all.contrasts[2 , i] ]
-                        de.table <- matrixTests::row_wilcoxon_twosample(data.x, data.y)[ , c('obs.x', 'obs.y', 'pvalue')]
+            all.possible.contrasts <- makeContrasts(
+                contrasts = all.possible.contrasts,
+                levels = colnames(de.design)
+            )
+            all.limma.tests <- lapply(
+                levels(assay.names),
+                function(x){
+                    printColoredMessage(
+                        message = paste0('- Apply limma test on the "', x, '" data:'),
+                        color = 'orange',
+                        verbose = verbose
+                    )
+                    vfit <- lmFit(object = all.assays[[x]] , design = de.design)
+                    vfit  <- contrasts.fit(fit = vfit , contrasts = all.possible.contrasts)
+                    efit <- eBayes(fit = vfit)
+                    p.values <- as.data.frame(efit$p.value)
+                    p.values.list <- lapply(
+                        colnames(p.values),
+                        function(x){
+                           d <- p.values[ , x, drop = FALSE]
+                           colnames(d) <- 'pvalue'
+                           d
+                           })
+                    names(p.values.list) <- gsub('-', '&', colnames(p.values))
+                    p.values.list
                     })
-                names(de.results) <- sapply(
-                    1:ncol(all.contrasts),
-                    function (x)
-                        paste(all.contrasts[1 , x], all.contrasts[2 , x], sep = '&')
-                )
-                return(de.results)
-            })
-        names(all.wilcoxon.tests) <- levels(assay.names)
+            names(all.limma.tests) <- levels(assay.names)
+        }
+
 
         # Save the results ####
         printColoredMessage(
             message = '-- Save the Wilcoxon test results:',
             color = 'magenta',
-            verbose = verbose)
+            verbose = verbose
+            )
         ## add results to the SummarizedExperiment object ####
         if (isTRUE(save.se.obj)) {
             printColoredMessage(
@@ -188,17 +244,32 @@ computeDGE <- function(
                 color = 'blue',
                 verbose = verbose
             )
-            se.obj <- addMetricToSeObj(
-                se.obj = se.obj,
-                slot = 'Metrics',
-                assay.names = assay.names,
-                assessment.type = 'gene.level',
-                assessment = 'DGE',
-                method = 'Wilcoxon',
-                variables = variable,
-                file.name = 'p.values',
-                results.data = all.wilcoxon.tests
-            )
+            if(method == 'wilcoxon'){
+                se.obj <- addMetricToSeObj(
+                    se.obj = se.obj,
+                    slot = 'Metrics',
+                    assay.names = levels(assay.names),
+                    assessment.type = 'gene.level',
+                    assessment = 'DGE',
+                    method = 'Wilcoxon',
+                    variables = variable,
+                    file.name = 'p.values',
+                    results.data = all.wilcoxon.tests
+                )
+            }
+            if(method == 'limma'){
+                se.obj <- addMetricToSeObj(
+                    se.obj = se.obj,
+                    slot = 'Metrics',
+                    assay.names = levels(assay.names),
+                    assessment.type = 'gene.level',
+                    assessment = 'DGE',
+                    method = 'limma',
+                    variables = variable,
+                    file.name = 'p.values',
+                    results.data = all.limma.tests
+                )
+            }
             printColoredMessage(
                 message = paste0('- All the Wilcoxon results for indiviaul assay(s) are saved to the .',
                                  ' "se.obj@metadata$metric$AssayName$DGE" in the SummarizedExperiment object.'),
@@ -220,7 +291,11 @@ computeDGE <- function(
             printColoredMessage(message = '------------The computeDGE function finished.',
                                 color = 'white',
                                 verbose = verbose)
-            return(all.wilcoxon.tests = all.wilcoxon.tests)
+            if(method == 'wilcoxon' ){
+                return(all.wilcoxon.tests = all.wilcoxon.tests)
+            } else {
+                return(all.limma.tests = all.limma.tests)
+            }
         }
     }
     return(se.obj)
@@ -228,3 +303,10 @@ computeDGE <- function(
                         color = 'white',
                         verbose = verbose)
 }
+
+
+
+
+
+
+
